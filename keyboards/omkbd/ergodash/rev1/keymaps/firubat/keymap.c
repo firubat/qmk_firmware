270e67bbc70368fc023eabbdaadfef35d5621fae
Ur Ya'ar <firubat@tutamail.com>
2026-07-07 15:06:28 +0300

    Adjustments to NUM layer

--- keyboards/omkbd/ergodash/rev1/keymaps/firubat/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/firubat/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      C_S_T(KC_GRV),  KC_Q,       KC_W,    KC_E,       KC_R,     KC_T,     KC_EQL,                                  KC_MINS,       KC_Y,    KC_U,     KC_I,     KC_O,       KC_P,     CW_TOGG,
      KC_TAB,         KC_A,       KC_S,    KC_D,       KC_F,     KC_G,     KC_LBRC,                                 KC_RBRC,       KC_H,    KC_J,     KC_K,     KC_L,       KC_SCLN,  KC_QUOT,
      SC_LSPO,        KC_Z,       KC_X,    KC_C,       KC_V,     KC_B,     TD(TD_SPC_NAV),                                TG(_NUM),      KC_N,    KC_M,     KC_COMM,  KC_DOT,     KC_SLSH,  SC_RSPC,
-     KC_LCTL,        KC_LGUI,    C(KC_SPC),     KC_LALT,   KC_BSPC,  LT(_NUM,KC_BSLS), LT(_NUM,KC_ENT), LT(_NUM,KC_ENT),  LT(_NAV,KC_SPC), KC_DEL,  KC_RALT,  KC_ESC,   C_S_T(KC_BSLS),KC_RCTL ),
+     KC_LCTL,        KC_LGUI,    C(KC_SPC),     KC_LALT,   KC_BSPC,  LT(_NAV,KC_BSLS), LT(_NUM,KC_ENT), LT(_NUM,KC_ENT),  LT(_NAV,KC_SPC), KC_DEL,  KC_RALT,  KC_ESC,   C_S_T(KC_BSLS),KC_RCTL ),
 
 	[_NAV] = LAYOUT_4key(
      _______,     KC_F1,      KC_F2,    KC_F3,        KC_F4,         KC_F5,    XXXXXXX,                      KC_VOLU,       KC_F6,   KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,
@@ -35,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,    _______,    _______,          _______,               _______, KC_SPC, _______,     _______, _______, _______,            _______,       _______, _______, _______),
 
     [_NUM] = LAYOUT_4key(
-     _______,  KC_F1,      KC_F2,    KC_F3,      KC_F4,    KC_F5,    KC_LCBR,                      KC_RCBR,        KC_F6,   KC_F7,     KC_F8,   KC_F9,    KC_F10,  KC_F11,
-     _______,  KC_HASH,    KC_1,     KC_2,       KC_3,     KC_PLUS,  KC_LBRC,                      KC_RBRC,        KC_HOME, XXXXXXX,   KC_UP,   XXXXXXX,  XXXXXXX, KC_F12,
-     _______,  KC_DOT,     KC_4,     KC_5,       KC_6,     KC_MINS,  KC_LT,                        KC_GT,          KC_END,  KC_LEFT,   KC_DOWN, KC_RGHT,  KC_PGUP, _______,
-     _______,  KC_SLSH,    KC_7,     KC_8,       KC_9,     KC_0,     TG(_LED),                      TG(_NUM),        XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,  KC_PGDN, _______,
-     _______,  _______,    _______,        _______,       _______,  KC_SPC,  _______,    _______,_______, _______,            _______,        _______,  _______, _______),
+     _______,  KC_F1,      KC_F2,    KC_F3,      KC_F4,    KC_F5,    KC_EQL,                      KC_EQL,     KC_F6,     KC_F7,    KC_F8,   KC_F9,    KC_F10,  KC_F11,
+     _______,  KC_QUES,    KC_1,     KC_2,       KC_3,     S(KC_P),  KC_PLUS,                      KC_PLUS,    KC_QUES,   KC_1,     KC_2,    KC_3,     S(KC_P),  KC_F12,
+     _______,  KC_COMM,    KC_4,     KC_5,       KC_6,     KC_DOT,  KC_MINS,                      KC_MINS,    KC_COMM,   KC_4,     KC_5,    KC_6,     KC_DOT,  _______,
+     _______,  KC_SLSH,    KC_7,     KC_8,       KC_9,     KC_0,      TG(_LED),                 TG(_NUM),     KC_SLSH,   KC_7,     KC_8,    KC_9,     KC_0,     _______,
+     _______,  _______,    _______,        _______,       KC_BSPC,   KC_SPC,  _______,    _______,  _______, KC_DEL,          _______,        _______,  _______, _______),
 
     [_LED] = LAYOUT_4key(
      QK_BOOT,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MODE_TWINKLE,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
